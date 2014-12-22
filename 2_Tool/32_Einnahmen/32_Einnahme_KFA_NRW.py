6a3f529731f70b384bcc676c6e2f14bd477cded4
RegioProjektCheck <regioprojektcheck@gmail.com>
2014-12-22 17:05:49 +0100

    32_Einnahmen

---
+++ 2_Tool/32_Einnahmen/32_Einnahme_KFA_NRW.py
@@ -0,0 +1,233 @@
+﻿# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------------
+# 32_Einnahme_KFA_NRW.py
+#
+# Description:
+#
+#Author:
+#Philippe Rieffel
+# ---------------------------------------------------------------------------
+
+import arcpy, Einnahmen_lib as e, time, gc,os, sys
+#Überflüssige Variablen entfernen
+gc.collect()
+
+#Projektparameter festlegen
+#Parameter für das Eingabefenster definieren
+projektname = arcpy.GetParameterAsText(0)
+startjahr = arcpy.GetParameterAsText(1)
+endjahr = arcpy.GetParameterAsText(2)
+
+
+#aktueller Pfad
+wcd = os.getcwd()
+rootPfad = str(sys.path[0]).split("2_Tool")[0]
+
+#Pfade einrichten
+workspace_basis = os.path.join(rootPfad,"1_Basisdaten","FGBD_01_Basisdaten_deutschland.gdb")
+workspace_projekt = os.path.join(rootPfad,"3_Projekte", projektname,"FGDB_11_Definition_Projekt_",projektname+'.gdb')
+workspace_ergebnisse = os.path.join(rootPfad,"3_Projekte",projektname,"FGDB_21_Einnahmen_Projekt_"+projektname+".gdb")
+pfad_ewz_prognose = os.path.join(rootPfad,"3_Projekte",projektname,"FGDB_22_BevModellierung_"+projektname+".gdb","bevoelkerung")
+pfad_wanderungsraten = os.path.join(rootPfad,"3_Projekte",projektname,"FGDB_22_BevModellierung_"+projektname+".gdb","wanderungsraten")
+
+
+#01: AGS aus Lage der Projektfläche ermitteln
+#Den AGS aus der Lage der projektfläche im Raum ermitteln
+projektFlaeche = rootPfad + "\\3_Projekte\\" + projektname + "\\FGDB_11_Definition_Projekt_" + projektname + ".gdb\\Teilflaechen_Plangebiet"
+bkg = rootPfad + "\\1_Basisdaten\\FGBD_01_Basisdaten_deutschland.gdb\\bkg_gemeinden"
+
+#ags aus BKG Daten extrahieren, dafür Gemeinde selektieren, die von Planfläche geschnitten wird
+#1. Feature Layer aus den bkg-daten erstellen
+arcpy.MakeFeatureLayer_management(bkg,"bkg_lyr")
+
+#2.Spatial Select wo Planfläche bkg_lyr intersected
+arcpy.SelectLayerByLocation_management("bkg_lyr", "INTERSECT", projektFlaeche)
+
+#Wenn Flaeche = 1, ags extrahieren
+n = arcpy.GetCount_management("bkg_lyr").getOutput(0)
+if(int(n) ==1 ):
+    gemeindeCursor = arcpy.SearchCursor("bkg_lyr")
+    for gemeinde in gemeindeCursor:
+        ags = gemeinde.AGS
+arcpy.AddMessage(ags)
+
+#Ueber die prognosejahre iterieren
+iteratorjahr = startjahr
+while iteratorjahr <=endjahr:
+    jahr = iteratorjahr
+
+    #Einwohnerzahltabelle öffnen, maennliche ezw extrahieren
+    geschlecht = "gesm"
+    where_clause = '"jahr" = ' + str(iteratorjahr) + " AND " + '"geschlecht" = ' + "'" + geschlecht + "'"
+    ewz_prognose = arcpy.SearchCursor(pfad_ewz_prognose,where_clause)
+    for row in ewz_prognose:
+        bev_m_alt000b03 = row.alt000b03
+        bev_m_alt003b06 =row.alt003b06
+        bev_m_alt006b10 =row.alt006b10
+        bev_m_alt010b15 =row.alt010b15
+        bev_m_alt015b18 =row.alt015b18
+        bev_m_alt018b20 =row.alt018b20
+        bev_m_alt020b25 =row.alt020b25
+        bev_m_alt025b30 =row.alt025b30
+        bev_m_alt030b35 =row.alt030b35
+        bev_m_alt035b40 =row.alt035b40
+        bev_m_alt040b45 =row.alt040b45
+        bev_m_alt045b50 =row.alt045b50
+        bev_m_alt050b55 =row.alt050b55
+        bev_m_alt055b60 =row.alt055b60
+        bev_m_alt060b65 =row.alt060b65
+        bev_m_alt065b75 =row.alt065b75
+        bev_m_alt075um =row.alt075um
+    del ewz_prognose, row
+
+    #Wanderungsraten extrahieren um Zugezogene zu ermitteln
+    where_clause = '"geschlecht" = ' + "'" + geschlecht + "'"
+    wanderungsraten = arcpy.SearchCursor(pfad_wanderungsraten, where_clause)
+    for row in wanderungsraten:
+        wand_m_alt000b03 =row.alt000b03
+        wand_m_alt003b06 =row.alt003b06
+        wand_m_alt006b10 =row.alt006b10
+        wand_m_alt010b15 =row.alt010b15
+        wand_m_alt015b18 =row.alt015b18
+        wand_m_alt018b20 =row.alt018b20
+        wand_m_alt020b25 =row.alt020b25
+        wand_m_alt025b30 =row.alt025b30
+        wand_m_alt030b35 =row.alt030b35
+        wand_m_alt035b40 =row.alt035b40
+        wand_m_alt040b45 =row.alt040b45
+        wand_m_alt045b50 =row.alt045b50
+        wand_m_alt050b55 =row.alt050b55
+        wand_m_alt055b60 =row.alt055b60
+        wand_m_alt060b65 =row.alt060b65
+        wand_m_alt065b75 =row.alt065b75
+        wand_m_alt075um =row.alt075um
+    del wanderungsraten, row
+
+    #Einwohnerzahltabelle öffnen, weibliche ezw extrahieren
+    geschlecht = "gesw"
+    where_clause = '"jahr" = ' + str(iteratorjahr) + " AND " + '"geschlecht" = ' + "'" + geschlecht + "'"
+    ewz_prognose = arcpy.SearchCursor(pfad_ewz_prognose,where_clause)
+    for row in ewz_prognose:
+        bev_w_alt000b03 = row.alt000b03
+        bev_w_alt003b06 =row.alt003b06
+        bev_w_alt006b10 =row.alt006b10
+        bev_w_alt010b15 =row.alt010b15
+        bev_w_alt015b18 =row.alt015b18
+        bev_w_alt018b20 =row.alt018b20
+        bev_w_alt020b25 =row.alt020b25
+        bev_w_alt025b30 =row.alt025b30
+        bev_w_alt030b35 =row.alt030b35
+        bev_w_alt035b40 =row.alt035b40
+        bev_w_alt040b45 =row.alt040b45
+        bev_w_alt045b50 =row.alt045b50
+        bev_w_alt050b55 =row.alt050b55
+        bev_w_alt055b60 =row.alt055b60
+        bev_w_alt060b65 =row.alt060b65
+        bev_w_alt065b75 =row.alt065b75
+        bev_w_alt075um =row.alt075um
+    del ewz_prognose, row
+
+    #Wanderungsraten extrahieren um Zugezogene zu ermitteln
+    where_clause = '"geschlecht" = ' + "'" + geschlecht + "'"
+    wanderungsraten = arcpy.SearchCursor(pfad_wanderungsraten, where_clause)
+    for row in wanderungsraten:
+        wand_w_alt000b03 =row.alt000b03
+        wand_w_alt003b06 =row.alt003b06
+        wand_w_alt006b10 =row.alt006b10
+        wand_w_alt010b15 =row.alt010b15
+        wand_w_alt015b18 =row.alt015b18
+        wand_w_alt018b20 =row.alt018b20
+        wand_w_alt020b25 =row.alt020b25
+        wand_w_alt025b30 =row.alt025b30
+        wand_w_alt030b35 =row.alt030b35
+        wand_w_alt035b40 =row.alt035b40
+        wand_w_alt040b45 =row.alt040b45
+        wand_w_alt045b50 =row.alt045b50
+        wand_w_alt050b55 =row.alt050b55
+        wand_w_alt055b60 =row.alt055b60
+        wand_w_alt060b65 =row.alt060b65
+        wand_w_alt065b75 =row.alt065b75
+        wand_w_alt075um =row.alt075um
+    del wanderungsraten, row
+
+    #Alle Altersgruppen und Geschlechter zur gesamten Einwohnerzahl fuer dieses Jahr zusammenrechnen
+    ewz = bev_m_alt000b03 + bev_m_alt003b06 + bev_m_alt006b10 + bev_m_alt010b15 + bev_m_alt015b18 + bev_m_alt018b20 + bev_m_alt020b25 + bev_m_alt025b30 + bev_m_alt030b35 + bev_m_alt035b40 + bev_m_alt040b45 + bev_m_alt045b50 + bev_m_alt050b55 + bev_m_alt055b60 + bev_m_alt060b65 + bev_m_alt065b75 + bev_m_alt075um +bev_w_alt000b03 + bev_w_alt003b06 + bev_w_alt006b10 + bev_w_alt010b15 + bev_w_alt015b18 + bev_w_alt018b20 + bev_w_alt020b25 + bev_w_alt025b30 + bev_w_alt030b35 + bev_w_alt035b40 + bev_w_alt040b45 + bev_w_alt045b50 + bev_w_alt050b55 + bev_w_alt055b60 + bev_w_alt060b65 + bev_w_alt065b75 + bev_w_alt075um
+
+    #Mit Hilfe der Wanderungsraten die Zugezogenen ermitteln
+    zgz = (bev_m_alt000b03*wand_m_alt000b03) + (bev_m_alt003b06*wand_m_alt003b06) + (bev_m_alt006b10*wand_m_alt006b10) + (bev_m_alt010b15*wand_m_alt010b15) + (bev_m_alt015b18*wand_m_alt015b18) + (bev_m_alt018b20*wand_m_alt018b20) + (bev_m_alt020b25*wand_m_alt020b25) + (bev_m_alt025b30*wand_m_alt025b30) + (bev_m_alt030b35*wand_m_alt030b35) + (bev_m_alt035b40*wand_m_alt035b40) + (bev_m_alt040b45*wand_m_alt040b45) + (bev_m_alt045b50*wand_m_alt045b50) + (bev_m_alt050b55*wand_m_alt050b55) + (bev_m_alt055b60*wand_m_alt055b60) + (bev_m_alt060b65*wand_m_alt060b65) + (bev_m_alt065b75*wand_m_alt065b75) + (bev_m_alt075um**wand_m_alt075um) + (bev_w_alt000b03*wand_w_alt000b03) + (bev_w_alt003b06*wand_w_alt003b06) + (bev_w_alt006b10*wand_w_alt006b10) + (bev_w_alt010b15*wand_w_alt010b15) + (bev_w_alt015b18*wand_w_alt015b18) + (bev_w_alt018b20*wand_w_alt018b20) + (bev_w_alt020b25*wand_w_alt020b25) + (bev_w_alt025b30*wand_w_alt025b30) + (bev_w_alt030b35*wand_w_alt030b35) + (bev_w_alt035b40*wand_w_alt035b40) + (bev_w_alt040b45*wand_w_alt040b45) + (bev_w_alt045b50*wand_w_alt045b50) + (bev_w_alt050b55*wand_w_alt050b55) + (bev_w_alt055b60*wand_w_alt055b60) + (bev_w_alt060b65*wand_w_alt060b65) + (bev_w_alt065b75*wand_w_alt065b75) + (bev_w_alt075um**wand_w_alt075um)
+    if zgz < 0:
+        zgz = 0
+
+    #Schuelerzahl ergibt sich aus der Altersgruppe 6-18
+    schueler = bev_m_alt006b10 + bev_m_alt010b15 + bev_m_alt015b18 + bev_w_alt006b10 + bev_w_alt010b15 + bev_w_alt015b18
+
+    #Berechnungsschritte laut Modell
+    Steuerkraftmesszahl = e.einkommenssteuerkraftmesszahl(ags, jahr) + e.grundsteuerkraftmesszahlA(ags, jahr) + e.grundsteuerkraftmesszahlB(ags, jahr) + e.umsatzsteuerkraftmesszahl(ags, jahr) + e.gewerbesteuerkraftmesszahl(ags, jahr) + e.gewerbesteuerumlagenmesszahl(ags, jahr)
+
+    Ausgangsmesszahl = float(e.hauptansatz(ewz, zgz)) + e.schueleransatz (schueler) + e.soziallastenansatz(ags, jahr) + e.zentralitaetsansatz(ags, jahr)
+
+    test = Ausgangsmesszahl - Steuerkraftmesszahl
+
+    Schluesselzuweisungen = test * 0.9
+    Einnahmen = Schluesselzuweisungen + e.investitionszuweisungen()
+
+    #Ausgabeblock für das Statusfenster (temporär)
+    Einnahmen = round(Einnahmen)
+    arcpy.AddMessage("Steuerkraftmesszahl: " + str(Steuerkraftmesszahl))
+    arcpy.AddMessage("Ausgangsmesszahl: " + str(Ausgangsmesszahl))
+    arcpy.AddMessage("Ausgangsmesszahl - Steuerkraftmesszahl: "+str(test))
+    arcpy.AddMessage("=========================================================================")
+    arcpy.AddMessage("Einnahmen aus dem kommunalen Finanzausgleich NRW: " + str(Einnahmen) + " Euro")
+    arcpy.AddMessage("=========================================================================")
+
+    #Das Ergebnis der "Ergebnisse" Tabelle hinzufügen
+    sql = "ags = " + ags + "AND parameter_jahr = " + jahr
+    table1 = rootPfad + "\\3_Projekte\\" + projektname +"\\FGDB_32_Einnahmen_Projekt_"+projektname+".gdb\\Ergebnisse_KFA"
+    #Überprüfen, ob schon ein Ergebniswert mit der Kombination aus ags und Jahr existiert
+    #Pro AGS und Jahr soll immer nur ein Ergebnis (neuestes Ergebnis) vorliegen
+    checkrows = arcpy.SearchCursor(table1, sql)
+    anzahl = 0
+    for row in checkrows:
+        anzahl = anzahl + 1
+    #arcpy.AddMessage("Anzahl = " + str(anzahl))
+    #wenn anzahl = 1, exisistiert ein Ergebnis, das Ergebnis wird überschrieben
+    #die "parameter" Spalten enthalten die Werte, die am Anfang in das Werkzeug eingegeben werden, zur besseren Nachvollziehbarkeit
+    if anzahl == 1:
+        Ergebnisse = arcpy.UpdateCursor(table1, sql)
+        for Ergebnis in Ergebnisse:
+            Ergebnis.Einnahmen = Einnahmen
+            lt = time.localtime()
+            year, monat, tag, stunde, minute = lt[0:5]
+            Ergebnis.timestamp =  str(tag) + "." + str(monat) + "." + str(year) + " - " + str(stunde) + ":" + str(minute)
+            Ergebnis.ags = ags
+            Ergebnis.parameter_jahr = jahr
+            Ergebnis.parameter_schuelerzahl = schueler
+            Ergebnis.parameter_einwohnerzahl = ewz
+            Ergebnis.parameter_zugezogene = zgz
+            Ergebnisse.updateRow(Ergebnis)
+            arcpy.AddMessage("Eintrag in Ergebnistabelle aktualisiert")
+            del Ergebnis, Ergebnisse
+    #wenn Anzahl = 0 existiert noch kein Ergebnis, eine neue Zeile wird angelegt
+    #die "parameter" Spalten enthalten die Werte, die am Anfang in das Werkzeug eingegeben werden, zur besseren Nachvollziehbarkeit
+    elif anzahl == 0:
+        Ergebnisse = arcpy.InsertCursor(table1)
+        Ergebnis = Ergebnisse.newRow()
+        Ergebnis.ags = ags
+        Ergebnis.Einnahmen = Einnahmen
+        lt = time.localtime()
+        year, monat, tag, stunde, minute = lt[0:5]
+        Ergebnis.timestamp =  str(tag) + "." + str(monat) + "." + str(year) + " - " + str(stunde) + ":" + str(minute)
+        Ergebnis.parameter_jahr = jahr
+        Ergebnis.parameter_schuelerzahl = schueler
+        Ergebnis.parameter_einwohnerzahl = ewz
+        Ergebnis.parameter_zugezogene = zgz
+        Ergebnisse.insertRow(Ergebnis)
+        arcpy.AddMessage("Eintrag in Ergebnistablle angelegt")
+        del Ergebnis, Ergebnisse
+    else:
+    #Wenn Anzahl weder 0 noch 1 liegen in der Ergebnistabelle mehrere Werte zu einem AGs vor, das ist so nicht gewollt und bedarf manuellen Eingreifens
+        arcpy.AddMessage("In der Ergebnistabelle liegen widersprüchliche Daten vor. Bitte überprüfen Sie die Ergebnistablle unter " + str(workspace_ergebnisse) + " auf Fehler" )
+
+
+    iteratorjahr += 1
+    gc.collect()
---
+++ 2_Tool/32_Einnahmen/32_Grundsteuer.py
@@ -0,0 +1,295 @@
+﻿# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      rieffel
+#
+# Created:     03.07.2013
+# Copyright:   (c) rieffel 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+import arcpy, Einnahmen_lib, os
+try:
+    i = 0
+    #Projektnamen
+    projektname = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Pfad anlegen
+    base_path = str(sys.path[0]).split("2_Tool")[0]
+    wcd = os.getcwd()
+    rootPfad = wcd.split("2_")[0] +"\\00_Entwicklungsumgebung"
+
+    #Den AGS aus der Lage der projektfläche im Raum ermitteln
+    projektFlaeche = rootPfad + "\\3_Projekte\\" + projektname + "\\FGDB_11_Definition_Projekt_" + projektname + ".gdb\\Teilflaechen_Plangebiet"
+    bkg = rootPfad + "\\1_Basisdaten\\FGBD_01_Basisdaten_deutschland.gdb\\bkg_gemeinden"
+    #ags aus BKG Daten extrahieren, dafür Gemeinde selektieren, die von Planfläche geschnitten wird
+    #1. Feature Layer aus den bkg-daten erstellen
+    arcpy.MakeFeatureLayer_management(bkg,"bkg_lyr")
+    #2.Spatial Select wo Planfläche bkg_lyr intersected
+    arcpy.SelectLayerByLocation_management("bkg_lyr", "INTERSECT", projektFlaeche)
+    #Wenn Flaeche = 1, ags extrahieren
+    n = arcpy.GetCount_management("bkg_lyr").getOutput(0)
+    if(int(n) ==1 ):
+        gemeindeCursor = arcpy.SearchCursor("bkg_lyr")
+        for gemeinde in gemeindeCursor:
+            ags = gemeinde.AGS
+    #arcpy.AddMessage(base_path)
+##    arcpy.AddMessage("1")
+
+##    #Eingabe: Kommune  (AGS?)
+##    ags = arcpy.GetParameterAsText(i)
+##    i=i+1
+##    #Eingabe: Flächengröße
+##    flaechengroesse = arcpy.GetParameterAsText(i)
+##    i=i+1
+
+#Ermittlung Flaechengroesse
+    flaechengroesse = 0
+    flaechen = arcpy.SearchCursor(projektFlaeche)
+    for flaeche in flaechen:
+        groesse = flaeche.Flaeche_HA
+        flaechengroesse = flaechengroesse + groesse
+##    arcpy.AddMessage("2")
+
+#Ermittlung Nettobauland
+    Flaechenbilanz_Planung = rootPfad + "\\3_Projekte\\" + projektname + "\\FGDB_11_Definition_Projekt_" + projektname + ".gdb\\" + "Flaechenbilanz_Planung_Prozent"
+    nettobauland_gesamt = 0
+    sql = "Flaechennutzung_S1 = 'Nettobauland'"
+    cursor = arcpy.SearchCursor(Flaechenbilanz_Planung,sql)
+##    arcpy.AddMessage("3")
+    for row in cursor:
+        nettobauland_temp = row.Flaeche_ha
+        nettobauland_gesamt = nettobauland_gesamt + nettobauland_temp
+    nettobauland = nettobauland_gesamt
+    #arcpy.AddMessage("4")
+##        #Eingabe: Nettobauland
+##        nettobauland = arcpy.GetParameterAsText(i)
+##        i=i+1
+    #Ermittlung zuvor landwirtschaftlich genutzter Fläche
+    vornutzungDetailsProzent = rootPfad + "\\3_Projekte\\" +projektname + "\\FGDB_11_Definition_Projekt_" + projektname + ".gdb\\Vornutzungen_Details_Prozent"
+    sql = "Vornutzungstyp = 'Ackerflaeche'"
+    cursor = arcpy.SearchCursor(vornutzungDetailsProzent, sql)
+    anteil = 0
+    teilflaeche = ""
+    landwirtschaftlicheVornutzung = 0
+    for row in cursor:
+        anteil = row.Prozent_der_Flaeche
+        anteil = anteil / 100
+        teilflaeche = row.Teilflaeche_Plangebiet
+        sql = "Teilflaeche_Plangebiet = '"+teilflaeche+"'"
+        cursor2 = arcpy.SearchCursor(Flaechenbilanz_Planung,sql)
+        for row in cursor2:
+            temp = row.Flaeche_ha * anteil
+            landwirtschaftlicheVornutzung = landwirtschaftlicheVornutzung + temp
+    #arcpy.AddMessage("5")
+
+    #Eingabe: zuvor landwirtschaftliche Nutzung
+##    landwirtschaftlicheVornutzung = arcpy.GetParameterAsText(i)
+##    i=i+1
+
+    #Berechnung: davon zuvor bauliche Nutzung (Grundsteuer B)
+    baulicheVornutzung = float(flaechengroesse) - float(landwirtschaftlicheVornutzung)
+
+    #Eingabe: Stand der Aufsiedlung
+    standAufsiedlung = arcpy.GetParameterAsText(i)
+    i=i+1
+
+    #Monatsrohmieten 1964, falls nicht bekannt 2010 (m2 Preise)
+    #Eingabe: Wohnungen
+    mieteWohnungen = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Einfamilienhäuser
+    mieteEinfamilienhaeuser = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Zweifamilienhäuser
+    mieteZweifamilienhaeuser = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Reihenhäuser
+    mieteReihenhaeuser = arcpy.GetParameterAsText(i)
+    i=i+1
+
+    #Eingabe: Baulandpreis unbebaute Grundstücke (Wohnbauland 1964, falls nicht bekannt 2010)
+    baulandpreisUnbebaut = arcpy.GetParameterAsText(i)
+    i=i+1
+
+    ##Gebäudezusammensetzung
+    #Eingabe: Gebäudeanzahl Einfamilienhaus
+    gebaeudeanzahlEinfamilienhaus = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Grundstücksfläche je Gebäude Einfamilienhaus
+    grundstuecksflaecheEinfamilienhaus = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Berechnung: Mieten 1964 Einfamilienhaus (Anzeigen und Berechnen in der Eingabemaske(Validierung)
+    miete1964Einfamilienhaus = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Gebäudeanzahl Zweifamilienhaus
+    gebaeudeanzahlZweifamilienhaus =  arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Grundstücksfläche je Gebäude Zweifamilienhaus
+    grundstuecksflaecheZweifamilienhaus = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Berechnung: Mieten 1964 Zweifamilienhaus (Anzeigen und Berechnen in der Eingabemaske(Validierung)
+    miete1964Zweifamilienhaus = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Gebäudeanzahl Reihenhaus
+    gebaeudeanzahlReihenhaus = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Grundstücksfläche je Gebäude Reihenhaus
+    grundstuecksflaecheReihenhaus = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Berechnung: Mieten 1964 Reihenhaus (Anzeigen  und Berechnen in der Eingabemaske (Validierung)
+    miete1964Reihenhaus = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Gebäudeanzahl Mehrfamilienhaus
+    gebaeudeanzahlMehrfamilienhaus = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Grundstücksfläche je Gebäude Mehrfamilienhaus
+    grundstuecksflaecheMehrfamilienhaus = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Berechnung: Mieten 1964 Mehrfamilienhaus (Anzeigen und Berechnen in der Eingabemaske (Validierung)
+    miete1964Mehrfamilienhaus = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe : Anzahl der Garagen
+    anzahlGaragen = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Miete je Garage
+    mieteGarage = arcpy.GetParameterAsText(i)
+    i=i+1
+
+    ##Berechnung: Größe unbebaute Fläche (baureifes Bauland)
+    #Nettobauland - ((Häuser * Hausflächen) * Aufsiedlungsstand/100/10000))
+    unbebauteFlaeche = float(nettobauland) - ((float(gebaeudeanzahlEinfamilienhaus) * float(grundstuecksflaecheEinfamilienhaus))+(float(gebaeudeanzahlZweifamilienhaus)*float(grundstuecksflaecheZweifamilienhaus))+(float(gebaeudeanzahlMehrfamilienhaus)*float(grundstuecksflaecheMehrfamilienhaus))+(float(gebaeudeanzahlReihenhaus)*float(grundstuecksflaecheReihenhaus)))* float(standAufsiedlung)/100/10000
+    #arcpy.AddMessage("6")
+    ##GewerblichesVorhaben
+    #Eingabe: Art des Gebäudes (Als DropdownListe in der Validierung umsetzen)
+    gewerbeGebaeudeArt = arcpy.GetParameterAsText(i)
+    i=i+1
+    gewerbeGebaeudeArt = gewerbeGebaeudeArt.encode("utf-8")
+    #EIngabe: Anteil der versiegelten Fläche
+    GewerbeVersiegelteFlaeche = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Grundfläche des Gebäudes
+    GewerbeGrundflaeche = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Zahl der Etagen
+    GewerbeEtagenzahl = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Berechnung: Nutzfläche (grundfläche * Etagen)
+    GewerbeNutzflaeche = float(GewerbeGrundflaeche) * float(GewerbeEtagenzahl)
+    #Extraktion: Geschosshöhe (nach Gebäudeart)
+    if(gewerbeGebaeudeArt == "Buero- und Verwaltungsgebaeude"):
+        Gewerbegeschosshoehe = 4
+    elif(gewerbeGebaeudeArt == "Fabrik- und Werkstattgebaeude"):
+        Gewerbegeschosshoehe = 7.8
+    elif(gewerbeGebaeudeArt == "Handelsgebaeude"):
+        Gewerbegeschosshoehe =7.9
+    #arcpy.AddMessage("7")
+
+
+    #Extraktion: Raummeterpreise und Wertezahlen hardcoden, in der EingabeMaske als parameter darstellen und änderbar machen
+    GewerbeRaummeterpreise =  arcpy.GetParameterAsText(i)
+    i=i+1
+    GewerbeWertezahlen = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Eingabe: Baulandpreise  unbebaute Grundstücke (Gewerbe)
+    GewerbeBaulandpreise = arcpy.GetParameterAsText(i)
+    i=i+1
+    #Hebesätze extrahieren
+    #Grundsteuer A & B Sätze
+    #arcpy.AddMessage("8")
+    sql = "ags = " + "'"+ags+"'"
+    #arcpy.AddMessage(sql)
+    table3 = base_path+r'1_Basisdaten\FGBD_01_Basisdaten_deutschland.gdb\Steuersaetze_2010'
+    #arcpy.AddMessage(table3)
+    #arcpy.AddMessage(table3)
+    rows = arcpy.SearchCursor(table3, sql)
+    for row in rows:
+        #arcpy.AddMessage("drin")
+        GrundsteuerAHebesatz = row.getValue("Grundsteuer_A")
+        #arcpy.AddMessage(GrundsteuerAHebesatz)
+        GrundsteuerBHebesatz = row.getValue("Grundsteuer_B")
+        #arcpy.AddMessage(GrundsteuerBHebesatz)
+    del rows
+
+
+
+    #arcpy.AddMessage(GrundsteuerAHebesatz)
+    #arcpy.AddMessage(GrundsteuerBHebesatz)
+
+    #Einwohnerzahl extrahieren
+    sql = "ags = " + "'"+ags+"'"
+    table3 = base_path+r'1_Basisdaten\FGBD_01_Basisdaten_deutschland.gdb\Bevoelkerungsdaten_2010'
+    rows = arcpy.SearchCursor(table3, sql)
+    for row in rows:
+        einwohner = row.getValue("Einwohnerzahl")
+    del rows
+    #arcpy.AddMessage(einwohner)
+    #arcpy.AddMessage("9")
+    #Groessenklasse der Kommune berechnen
+    groessenklasse = Einnahmen_lib.groessenklasseKommune(einwohner)
+   # arcpy.AddMessage("10")
+    #Grundsteuer A berechnen
+    GrundsteuerA = Einnahmen_lib.grundsteuerA(landwirtschaftlicheVornutzung,GrundsteuerAHebesatz)
+    #arcpy.AddMessage("11")
+    #arcpy.AddMessage(GrundsteuerA)
+    #GrundsteuerB berechnen
+    #Teil Wohnen
+    GrundsteuerBwohnen = Einnahmen_lib.grundsteuerBwohnen(gebaeudeanzahlEinfamilienhaus,miete1964Einfamilienhaus,gebaeudeanzahlZweifamilienhaus,miete1964Zweifamilienhaus,gebaeudeanzahlReihenhaus,miete1964Reihenhaus,gebaeudeanzahlMehrfamilienhaus,miete1964Mehrfamilienhaus,anzahlGaragen,mieteGarage,standAufsiedlung,groessenklasse,GrundsteuerBHebesatz)
+    #arcpy.AddMessage("12")
+    #arcpy.AddMessage(GrundsteuerBwohnen)
+    #Teil unbebaut
+    grundsteuerBunbebaut = Einnahmen_lib.grundsteuerBunbebaut(unbebauteFlaeche,baulandpreisUnbebaut,GrundsteuerBHebesatz)
+    #arcpy.AddMessage("13")
+    #arcpy.AddMessage(grundsteuerBunbebaut)
+    #Teil Gewerbe
+    grundsteuerBGewerbeEinnahme = Einnahmen_lib.grundsteuerBGewerbe(GewerbeNutzflaeche,GewerbeGrundflaeche,Gewerbegeschosshoehe,standAufsiedlung,GewerbeVersiegelteFlaeche,GewerbeRaummeterpreise,GewerbeBaulandpreise,GewerbeWertezahlen,flaechengroesse,GrundsteuerBHebesatz)
+    #arcpy.AddMessage("14")
+    #arcpy.AddMessage(grundsteuerBGewerbeEinnahme)
+    #Teil gewerbe unbebaut
+    grundsteuerBunbebautGewerbeEinnahmen = Einnahmen_lib.grundsteuerBunbebautGewerbe(nettobauland,standAufsiedlung,baulandpreisUnbebaut,GrundsteuerBHebesatz)
+    #arcpy.AddMessage("15")
+    #Zusammenrechnung zu den Grundsteuereinnahmen
+    Grundsteuereinnahmen = GrundsteuerA + GrundsteuerBwohnen + grundsteuerBunbebaut + grundsteuerBGewerbeEinnahme +grundsteuerBunbebautGewerbeEinnahmen
+    arcpy.AddMessage("Grundsteuereinnahmen: "+ str(Grundsteuereinnahmen))
+
+    #Grundsteuer in Tabelle abspeichern
+    sql = "ags = " + ags
+    #arcpy.AddMessage(sql)
+    #ergebnistabelle = base_path + r"Projekte\" +projektname+"\FGDB_32_Einnahmen_Tool.gdb\Grundsteuer"
+    ergebnistabelle = os.path.join(base_path,"3_Projekte",projektname,"FGDB_32_Einnahmen_Projekt_"+projektname+".gdb","Grundsteuer")
+    try:
+        Update = arcpy.UpdateCursor(ergebnistabelle,sql)
+        #arcpy.AddMessage("16")
+        count = 0
+        for row in Update:
+            count = count +1
+        #arcpy.AddMessage(count)
+        if(count>0):
+            for row in Update:
+                row.grundsteuer_einnahmen = float(Grundsteuereinnahmen)
+                Update.updateRow(row)
+        else:
+            Insert = arcpy.InsertCursor(ergebnistabelle)
+            row = Insert.newRow()
+            row.ags = ags
+            row.grundsteuer_einnahmen = float(Grundsteuereinnahmen)
+            Insert.insertRow(row)
+            del Insert, row
+        try:
+            del Update
+        except Exception as e:
+            arcpy.AddMessage(e)
+        arcpy.AddMessage("Grundsteuereinnahmen gespeichert")
+    except Exception as e:
+
+        arcpy.AddMessage(e)
+except Exception as e:
+    arcpy.AddMessage(e)
+#Bis auf den Rückrechnungsindex für Mietpreise von 2010 ist alles funktionsfähig implementiert
