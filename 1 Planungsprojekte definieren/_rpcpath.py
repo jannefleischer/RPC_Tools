0e6eaa053a7bb68bc543231de11e842e4b43fa09
Jens-Martin <gutsche@ggr-planung.de>
2017-02-14 18:37:03 +0100

    old Toolboxes removed

--- 1 Planungsprojekte definieren/_rpcpath.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import sys
-from os.path import abspath, dirname, join
-BASE_PATH = dirname(dirname(abspath(__file__)))
-sys.path.append(join(BASE_PATH, '4 Programminterne Daten'))
