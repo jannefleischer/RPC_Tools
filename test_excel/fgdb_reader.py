bea024d7feb6387d123e88d7bd741e8ebb3e013c
Anne Kis <AK@ggrplanung.onmicrosoft.com>
2017-04-12 17:15:38 +0200

    Create Classes get_results and edit_excel

---
+++ test_excel/fgdb_reader.py
@@ -0,0 +1,55 @@
+import arcpy
+from arcpy import env
+import numpy as np
+
+
+#gdb_path = r"C:\ProjektCheck\3 Benutzerdefinierte Projekte" + \
+#"\Wir_wollen_Diagramme\FGDB_Definition_Projekt.gdb"
+#test_data = 'Wohnen_WE_in_Gebaeudetypen'
+#columns = np.array(["WE", "EW_je_WE"])
+
+class Read_FGDB(object):
+    
+    
+    def __init__(self, gdb_path, table, columns, clause):
+        self.gdb_path = gdb_path
+        self.table = table
+        self.columns = columns
+        self.clause = clause
+        self.result_block = []
+        
+        
+    def get_result_block(self):
+        """
+        Extract data from gdb.
+        
+        Parameters
+        ----------
+        gdb_path : str
+            Path to GDB
+        table : str
+            Table inside the GDB
+        columns : np.array of str
+            Columns inside the table
+        
+        Returns
+        -------
+        result_block : np.array of str
+            Extracted data from GDB
+        """
+        env.workspace = self.gdb_path
+        result_block = self.columns
+        with arcpy.da.SearchCursor(self.table, self.columns, 
+                                   where_clause=self.clause) as cur:
+            for we, ew_je_ew in cur:
+                new_row = np.array([we, ew_je_ew])
+                result_block = np.vstack((result_block, new_row))
+        
+        self.result_block = result_block
+
+
+    def sort_results_by_col(self, col, header=1):
+        """
+        """
+        self.result_block[header:, :] = self.result_block[header:, :]\
+            [self.result_block[header:, col].argsort()]
--- test_excel/test_excel_file.py
+++ test_excel/test_excel_file.py
@@ -5,187 +5,198 @@ import time
 import win32com.client
 from openpyxl.utils import get_column_letter
 import numpy as np
-
-# file-names
-filename = 'demo.xlsx'
-filename2 = r'C:\ProjektCheck\test_excel\demo.xlsx'
-input_block = np.diag(np.arange(6))
-input_block[0,:] = np.ones(6)
+from fgdb_reader import Read_FGDB
 
 
-def insert_cell_block(filename, sh_name, upper_left, cell_block):
+
+class Edit_Excel(object):
     """
-    Overwrite a cell-block in an Excel-file.
+    Class contains methods for editing an Microsoft Excel file.
     
-    Parameters
+    Attributes
     ----------
     filename : str
         Path to the Excel-file
-    sh_name : str
-        Name of the worksheet where the cells will 
-    upper_left : list of int
-        Indices of the upper-left cell that will be overwritten.
-        Cell A1 equals upper_left = [1, 1].
-    cell_block : Matix of str or float or int
-        Cell-block that will be copied to the Excel-file.
     
     """
-    wb = openpyxl.load_workbook(filename)
-    ws1 = wb.active
-    ws1.title = "sheet1"
-    block_cols, block_rows = np.shape(cell_block)
-    start_row = upper_left[0]
-    start_col = upper_left[1]
-    for row in range(block_rows):
-        for col in range(block_cols):
-            cell_id = get_column_letter(start_col+col) + str(start_row+row)
-            ws1[cell_id] = input_block[row, col]
-    wb.save(filename)
-    wb.close()
-
-insert_cell_block(filename, [4, 8], input_block)
-
-def append_rows(filename, rows):
-    """
-    Write and save new lines in .xlsx file.
     
-    Parameters
-    ----------
-    filename : str
-        Path to the Excel file
-    
-    """
-    wb = openpyxl.load_workbook(filename)
-    ws1 = wb.active
-    ws1.title = "range names"
-    ws1.append((rows, rows))
-    wb.save(filename)
-    wb.close()
-    
-  
-def clear_cellblock(filename, upper_left, lower_right):
-    """
-    clear the all cells between upper_left and lower_right
-    """
-    wb = openpyxl.load_workbook(filename)
-    sh = wb.get_sheet_by_name("range names")
-    for row in sh[upper_left+':'+lower_right]:
-        for cell in row:
-            cell.value = None
-    wb.save(filename)
-    wb.close()    
-    
-    
-def clear_cellblock_2(filename, upper_left, lower_right):
-    """
-    delete all rows between first an last
-    """
-    excel = win32com.client.gencache.EnsureDispatch("Excel.Application")
-    excel.Visible = 0
-    wb = excel.Workbooks.Open(filename)
-    excel.Range(upper_left+':'+lower_right).Select()
-    excel.Selection.ClearContents()
-    wb.Save()
-    wb.Close()
-    
-    
-def delete_rows(filename, first, last):
-    """
-    All rows from first to last are removed completely
-    """
-    excel = win32com.client.gencache.EnsureDispatch("Excel.Application")
-    excel.Visible = 0
-    wb = excel.Workbooks.Open(filename)
-    sh = wb.ActiveSheet
-    rangeObj = sh.Range('A'+str(first)+':A'+str(last))
-    rangeObj.EntireRow.Delete()    
-    wb.Save()
-    wb.Close() 
-    
-    
-def delete_columns(filename, first, last):
-    """
-    All columns from first to last are removed completely
-    """
-    excel = win32com.client.gencache.EnsureDispatch("Excel.Application")
-    excel.Visible = 1
-    wb = excel.Workbooks.Open(filename)
-    sh = wb.ActiveSheet
-    rangeObj = sh.Range(first+'1:'+last+'1')
-    rangeObj.EntireColumn.Delete()    
-    wb.Save()
-    wb.Close() 
-    
-    
-def open_file_in_excel(path):
-    """
-    Open .xlsx file in Excel and wait for 5 seconds to check if saving works
-    """
-    # open demo.xlsx file in Excel
-    xl = win32com.client.Dispatch("Excel.Application")
-    xl.Visible = True
-    xl.Workbooks.Open(path)
-
-    time.sleep(5)
+    def __init__(self, filename):
+        self.filename = filename
 
 
-def check_if_file_is_open(path):
-    """
-    Check if a file is opened in Excel.
-    If file is open: Save and close
-    """
-    excel = win32com.client.Dispatch("Excel.Application")
-    if excel.Workbooks.Count > 0:
-        for i in range(1, excel.Workbooks.Count+1):
-            #print(excel.Workbooks.Item(i).Name, filename)
-            if excel.Workbooks.Item(i).Name == filename:
-                wb = excel.Workbooks.Item(i)
-                wb.Save()
-                wb.Close()
-                break    
+    def insert_cell_block(self, upper_left, cell_block, header=0):
+        """
+        Overwrite a cell-block in an Excel-file.
+        
+        Parameters
+        ----------
+        sh_name : str
+            Name of the worksheet where the cells will 
+        upper_left : list of int
+            Indices of the upper-left cell that will be overwritten.
+            Cell A1 equals upper_left = [1, 1].
+        cell_block : Matix of str or float or int
+            Cell-block that will be copied to the Excel-file.
+        header : int, optional
+            Number of ignored head rows.
+        """
+        cell_block = np.delete(cell_block, (header), axis=0)
+        wb = openpyxl.load_workbook(self.filename)
+        ws1 = wb.active
+        ws1.title = "sheet1"
+        block_cols, block_rows = np.shape(cell_block)
+        start_row = upper_left[0]
+        start_col = upper_left[1]
+        for row in range(block_rows):
+            for col in range(block_cols):
+                cell_id = get_column_letter(start_col+col) + str(start_row+row)
+                ws1[cell_id] = cell_block[row, col]
+        wb.save(self.filename)
+        wb.close()
+    
+    
+    
+    def append_rows(self, rows):
+        """
+        Write and save new lines in .xlsx file.
+        
+        Parameters
+        ----------
+        rows : np.array of str or int or float
+            Rows that will be appended to the Excel-file
+            
+        """
+        wb = openpyxl.load_workbook(self.filename)
+        ws1 = wb.active
+        ws1.title = "range names"
+        for i in range(len(rows[:, 1])):
+            ws1.append(rows[i, :])
+        wb.save(self.filename)
+        wb.close()
+        
+      
+    def clear_cellblock(self, upper_left, lower_right):
+        """
+        clear the all cells between upper_left and lower_right
+        """
+        wb = openpyxl.load_workbook(self.filename)
+        sh = wb.get_sheet_by_name("range names")
+        for row in sh[upper_left+':'+lower_right]:
+            for cell in row:
+                cell.value = None
+        wb.save(self.filename)
+        wb.close()    
+        
+        
+    def clear_cellblock_2(self, upper_left, lower_right):
+        """
+        delete all rows between first an last
+        """
+        excel = win32com.client.gencache.EnsureDispatch("Excel.Application")
+        excel.Visible = 0
+        wb = excel.Workbooks.Open(self.filename)
+        excel.Range(upper_left+':'+lower_right).Select()
+        excel.Selection.ClearContents()
+        wb.Save()
+        wb.Close()
+        
+        
+    def delete_rows(self, first, last):
+        """
+        All rows from first to last are removed completely
+        """
+        excel = win32com.client.gencache.EnsureDispatch("Excel.Application")
+        excel.Visible = 0
+        wb = excel.Workbooks.Open(self.filename)
+        sh = wb.ActiveSheet
+        rangeObj = sh.Range('A'+str(first)+':A'+str(last))
+        rangeObj.EntireRow.Delete()    
+        wb.Save()
+        wb.Close() 
+        
+        
+    def delete_columns(self, first, last):
+        """
+        All columns from first to last are removed completely
+        """
+        excel = win32com.client.gencache.EnsureDispatch("Excel.Application")
+        excel.Visible = 1
+        wb = excel.Workbooks.Open(self.filename)
+        sh = wb.ActiveSheet
+        rangeObj = sh.Range(first+'1:'+last+'1')
+        rangeObj.EntireColumn.Delete()    
+        wb.Save()
+        wb.Close() 
+        
+        
+    def open_file_in_excel(self):
+        """
+        Open .xlsx file in Excel and wait for 5 seconds to check if saving works
+        """
+        # open demo.xlsx file in Excel
+        xl = win32com.client.Dispatch("Excel.Application")
+        xl.Visible = True
+        xl.Workbooks.Open(self.filename)
+    
+        time.sleep(5)
+    
+    
+    def check_if_file_is_open(self):
+        """
+        Check if a file is opened in Excel.
+        If file is open: Save and close
+        """
+        excel = win32com.client.Dispatch("Excel.Application")
+        if excel.Workbooks.Count > 0:
+            for i in range(1, excel.Workbooks.Count+1):
+                #print(excel.Workbooks.Item(i).Name, filename)
+                if excel.Workbooks.Item(i).Name == self.filename:
+                    wb = excel.Workbooks.Item(i)
+                    wb.Save()
+                    wb.Close()
+                    break    
 
         
         
-        
-        
-
-#delete_rows(filename2, 1, 4)
-
-
-
-
-#start = time.time()
-#clear_cellblock_2(filename2, 'A1', 'N2000')
-#print(time.time() - start)
-#time.sleep(3)
-#start = time.time()
-#clear_cellblock(filename, 'A1', 'N2000')
-#print(time.time() - start)
-#write_some_lines(filename, "before opening Excel")
-#open_file_in_excel(filename2)
-#check_if_file_is_open(filename2)
-#time.sleep(5)
-#write_some_lines(filename, "after opening Excel")
-
-print('done')
-
-
-
-
-
-"""
-MAY BE USEFUL LATER:
-
-try:
-    ws1.append(("after opening excel", "after opening excel"))
-    wb.save(filename)
-except:
-    print('file is already opened')
-# not used: opens demo.xlsx file
-#os.system('start excel.exe "%s\\demo.xlsx"' % (sys.path[0], ))
-"""
-
-
+if __name__ == '__main__':        
+    
+    
+    # Variables:
+    filename = 'demo.xlsx'
+    filename2 = r'C:\ProjektCheck\test_excel\demo.xlsx'
+    gdb_path = r"C:\ProjektCheck\3 Benutzerdefinierte Projekte" + \
+    "\Wir_wollen_Diagramme\FGDB_Definition_Projekt.gdb"
+    test_data = 'Wohnen_WE_in_Gebaeudetypen'
+    columns = np.array(["WE", "EW_je_WE"])
+    
+    Results = Read_FGDB(gdb_path, test_data, columns, 'WE>0 and WE<20')
+    Results.get_result_block()
+    Results.sort_results_by_col(1)
+    cell_block = Results.result_block
+    
+    Excel_Editor = Edit_Excel(filename2)
+    Excel_Editor.insert_cell_block([4, 8], cell_block)
+    Excel_Editor.open_file_in_excel()
+    
+    print('done')
+    
+    
+    
+    
+    
+    """
+    MAY BE USEFUL LATER:
+    
+    try:
+        ws1.append(("after opening excel", "after opening excel"))
+        wb.save(filename)
+    except:
+        print('file is already opened')
+    # not used: opens demo.xlsx file
+    #os.system('start excel.exe "%s\\demo.xlsx"' % (sys.path[0], ))
+    """
+    
+    
 
 
 
