83b20e83d8256ed830cf08ef1b42a1114772402b
MaxBo <max.bohnet@tuhh.de>
2017-04-14 19:58:14 +0200

    Projektauswahl shows Teilflächen-Layer as output now

--- 4 Programminterne Daten/rpctools/definitions/projektverwaltung/projektauswahl.py
+++ 4 Programminterne Daten/rpctools/definitions/projektverwaltung/projektauswahl.py
@@ -1,8 +1,13 @@
 from rpctools.utils.params import Tool
+from rpctools.definitions.projektverwaltung.T1_Projektverwaltung \
+     import Projektverwaltung
 
 
-class Projektauswahl(Tool):
+class Projektauswahl(Projektverwaltung):
+    _param_projectname = 'active_project'
+    _dbname = 'FGDB_Definition_Projekt.gdb'
 
     def run(self):
         self.parent_tbx.config.active_project = self.par.active_project.value
         self.parent_tbx.config.write()
+        self.add_output_new_project()
