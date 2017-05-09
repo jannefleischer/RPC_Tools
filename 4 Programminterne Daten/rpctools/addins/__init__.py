8f324b5b67a69508049ab50ccd047b9cf11c8169
ChrFr <christoph.franke@web.de>
2017-05-09 11:10:44 +0200

    Update: Undone last commit, no cross access between different classes via identifiers possible when packaging adding classes

--- 4 Programminterne Daten/rpctools/addins/__init__.py
+++
@@ -1,7 +0,0 @@
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
-
-from rpctools.addins.erschliessungsnetz import *
-from rpctools.addins.projektverwaltung import *
-from rpctools.addins.definitions import *
-from rpctools.addins.analyst import *
