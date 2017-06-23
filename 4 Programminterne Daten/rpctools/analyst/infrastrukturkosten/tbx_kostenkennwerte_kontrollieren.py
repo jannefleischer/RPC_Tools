18cf01667ac2d5274fa3a57f1dcfc5571f2eaa6e
Stefaan <hessmann.stefaan@googlemail.com>
2017-06-23 15:32:13 +0200

    new: function kostenkennwerte

--- 4 Programminterne Daten/rpctools/analyst/infrastrukturkosten/tbx_kostenkennwerte_kontrollieren.py
+++ 4 Programminterne Daten/rpctools/analyst/infrastrukturkosten/tbx_kostenkennwerte_kontrollieren.py
@@ -9,6 +9,8 @@ import time
 
 
 class KostenkennwerteKontrollieren(Tool):
+    _workspace = 'FGDB_Kosten.gdb'
+    _param_projectname = 'project'
 
     def add_outputs(self):
         pass
@@ -19,14 +21,54 @@ class KostenkennwerteKontrollieren(Tool):
 
     def kostenkennwerte(self):
         tbx = self.parent_tbx
+        # check if table Kostenkennwerte_Linienelemente contains content
+        df_costs_line_elements = tbx.table_to_dataframe(
+            'Kostenkennwerte_Linienelemente', workspace='FGDB_Kosten.gdb')
+        if len(df_costs_line_elements) != 0:
+            return
+        # calculate time factor
         current_year = int(time.strftime("%Y"))
-        frame_data = tbx.table_to_dataframe('Rahmendaten',
-                                            workspace='FGDB_Kosten_Tool',
+        df_frame_data = tbx.table_to_dataframe('Rahmendaten',
+                                            workspace='FGDB_Kosten_Tool.gdb',
                                             is_base_table=True)
-        time_factor = (1 + interest) * np.exp(current_year - reference_year)
+        interest = df_frame_data['Zins']
+        reference_year = df_frame_data['Stand_Kostenkennwerte']
+        time_factor = (1 + interest) ** (current_year - reference_year)
+        # get regional factor
+        ags = tbx.query_table('Projektrahmendaten',
+                              workspace='FGDB_Definition_Projekt.gdb',
+                              columns=['AGS'])[0][0]
+        regional_factor = tbx.table_to_dataframe(
+            'bkg_gemeinden', workspace='FGDB_Basisdaten_deutschland.gdb',
+            columns=['BKI_Regionalfaktor'], where="AGS='{}'".format(str(ags)),
+            is_base_table=True)
+        # fill table Kostenkennwerte_Linienelemente
+        regional_time_factor = time_factor * regional_factor.loc[:, 'BKI_Regionalfaktor']
+        rounding_factor = 5
+        df_networks = tbx.table_to_dataframe('Netze_und_Netzelemente',
+                                             workspace='FGDB_Kosten_Tool.gdb',
+                                             where="Typ='{}'".format('Linie'),
+                                             is_base_table=True)
+        # multiply with factors
+        df_networks.loc[:, ['Euro_EH', 'Cent_BU', 'Euro_EN']] *= \
+            regional_time_factor[0]
+        # round to 5
+        df_networks.loc[:, ['Euro_EH', 'Cent_BU', 'Euro_EN']] = \
+            self.round_df_to(
+                df_networks.loc[:, ['Euro_EH', 'Cent_BU', 'Euro_EN']],
+                rounding_factor)
 
-        return time_factor
+        tbx.dataframe_to_table('Kostenkennwerte_Linienelemente', df_networks,
+                               pkeys=['ID'], workspace='FGDB_Kosten.gdb',
+                               upsert=True)
+        return
 
+    def round_df_to(self, df, rounding_factor):
+        df = df / rounding_factor
+        df = df.apply(pd.Series.round)
+        df *= rounding_factor
+        df = df.astype('int')
+        return df
 
 class TbxKostenkennwerteKontrollieren(Tbx):
 
@@ -42,15 +84,31 @@ class TbxKostenkennwerteKontrollieren(Tbx):
         pass
 
     def _updateParameters(self, params):
+
         pass
 
     def _getParameterInfo(self):
-        pass
+        params = self.par
+        projekte = self.folders.get_projects()
+
+        # Projekt_auswählen
+        p = self.add_parameter('project')
+        p.name = u'Projekt_auswählen'.encode('cp1252')
+        p.displayName = u'Projekt auswählen'.encode('cp1252')
+        p.parameterType = 'Required'
+        p.direction = 'Input'
+        p.datatype = u'GPString'
+
+        p.filter.list = projekte
+        if projekte:
+            p.value = projekte[0]
+
+        return params
 
 
 if __name__ == '__main__':
     t = TbxKostenkennwerteKontrollieren()
     t._getParameterInfo()
-    t.set_active_project()
+    t.par.project.value = t.config.active_project
     t._updateParameters(None)
     t.execute()
