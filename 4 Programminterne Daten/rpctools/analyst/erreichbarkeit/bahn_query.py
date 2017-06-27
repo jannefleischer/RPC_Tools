720c16d25baadff223a3fd78ed7c85514434e215
ChrFr <christoph.franke@web.de>
2017-06-27 16:39:30 +0200

    Update: write stops near centers and planned area, request number of departures per stop

--- 4 Programminterne Daten/rpctools/analyst/erreichbarkeit/bahn_query.py
+++ 4 Programminterne Daten/rpctools/analyst/erreichbarkeit/bahn_query.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
 import requests
 import re
+from HTMLParser import HTMLParser
+from rpctools.utils.spatial_lib import Point
+import datetime
+
+
+class Stop(Point):
+    def __init__(self, x, y, name, id=None, epsg=4326):
+        super(Stop, self).__init__(x, y, id=id, epsg=epsg)
+        self.name = name
+
 
 class BahnQuery(object):
     reiseauskunft_url = 'http://reiseauskunft.bahn.de/bin/query.exe/dn' #?S="+bahn_start+"&Z="+ziel+"%20Hbf&date="+bahn_wochentag+"&time="+bahn_uhrzeit[0]+"&start=1"
     mobile_url = 'http://mobile.bahn.de/bin/mobil/query.exe/dox'
+    timetable_url = 'http://reiseauskunft.bahn.de/bin/bhftafel.exe/dn'#?ld=96242&country=DEU&rt=1&evaId=%s&bt=dep&time=24:00&maxJourneys=10000&date=%s&productsFilter=1111111111&max=10000&start=yes'
     
     routing_params = {
+        'start': 1,
         'S': '',
         'Z': '',
+        'date': '',
+        'time': ''
     }
     
     station_params = {
@@ -24,30 +38,99 @@ class BahnQuery(object):
         'look_y': 0
     }
     
-    def __init__(self):
-        pass
+    timetable_params = {
+        'ld': 96242,
+        'country': 'DEU',
+        'rt': 1,
+        'bt': 'dep',
+        'start': 'yes',
+        'productsFilter': 1111111111,
+        'max': 10000,
+        'maxJourneys': 10000,
+        'time': '24:00',
+        'date': '',
+        'evaId': 0,        
+    }
     
-    def closest_station(self, point, max_distance=2000, stopclass=1023):
-        """get closest station to given point (tuple of x,y; epsg: 4326)"""
+    date_pattern = '%d.%m.%Y'
+    
+    def __init__(self, date=None):
+        self.html = HTMLParser()
+        date = date or datetime.date.today()
+        self.date = date.strftime(self.date_pattern)
+        
+    def _to_db_coord(self, c):
+        return ("%.6f" % c).replace('.','')
+    
+    def _from_db_coord(self, c):
+        return c / 1000000.
+    
+    def stops_near(self, point, max_distance=2000, stopclass=1023, n=5000):
+        """get closest station to given point (tuple of x,y; epsg: 4326)
+        ordered by distance (ascending)
+        """
         # set url-parameters 
         params = self.station_params.copy()
         params['look_maxdist'] = max_distance
         params['look_stopclass'] = stopclass
-        x, y = point
-        params['look_x'] = ("%.6f" % x).replace('.','')
-        params['look_y'] = ("%.6f" % y).replace('.','')
+        if point.epsg != 4326:
+            raise ValueError('Point has to be in WGS84!')
+        params['look_x'] = self._to_db_coord(point.x)
+        params['look_y'] = self._to_db_coord(point.y)
         
         r = requests.get(self.mobile_url, params=params)
         # use regular expressions instead of parsing to xml, because html is
         # malformed and lxml is tough to install on all machines
-        regex = '<a class="uLine" .*>(.*?)</a>'
-        stops = re.findall(regex, r.text)
-        # ToDo: sort distances or do sth else with them? 
-        # (stations are already sorted ascending)
-        regex = '<div style="float:right">\\n(.*?) m\\n</div>'
-        distances = re.findall(regex, r.text)
         
-        return stops[0].decode('utf8')
+        regex = '<a class="uLine"(.*?)</a>'
+        rows = re.findall(regex, r.text)
+        
+        def parse_href_number(tag, href):
+            regex = '{tag}=(\d+)!'.format(tag=tag)
+            return re.search(regex, href).group(1)
+        
+        stops = []
+        
+        for row in rows:
+            regex = '>(.*)'
+            _name = re.search(regex, row).group(1)
+            if not _name:
+                continue
+            _x = int(parse_href_number('X', row))
+            _y = int(parse_href_number('Y', row))
+            _id = int(parse_href_number('id', row))
+            _dist = int(parse_href_number('dist', row))
+            stop = Stop(self._from_db_coord(_x), self._from_db_coord(_y),
+                        self.html.unescape(_name),
+                        id=_id, epsg=4326)
+            stop.dist = _dist
+            stops.append(stop)
+        
+        # response should be sorted by distances in first place, 
+        # but do it again because you can
+        stops_sorted = sorted(stops, key=lambda x: x.dist)        
+        if n < len(stops_sorted):
+            stops_sorted[:n]
+                
+        return stops
     
     def routing(origin, destination, times):
         params = {}
+        
+    def n_departures(self, stop_ids, max_journeys=10000):
+        '''stop_ids have to be hafas stuff'''
+        # set url-parameters 
+        params = self.timetable_params.copy()
+        params['date'] = self.date
+        params['maxJourneys'] = max_journeys
+        regex = 'id="journeyRow_(\d+)"'
+        n_departures = []
+        
+        for id in stop_ids:
+            params['evaId'] = id
+            r = requests.get(self.timetable_url, params=params)
+            n_rows = len(re.findall(regex, r.text))
+            n_departures.append(n_rows)
+            #sleep(0.5)
+            
+        return n_departures
--- 4 Programminterne Daten/rpctools/analyst/erreichbarkeit/tbx_ZentraleOrteOEPNV.py
+++ 4 Programminterne Daten/rpctools/analyst/erreichbarkeit/tbx_ZentraleOrteOEPNV.py
@@ -9,15 +9,41 @@ from rpctools.utils.encoding import encode
 from rpctools.utils.spatial_lib import get_project_centroid, points_within, Point
 from rpctools.analyst.erreichbarkeit.bahn_query import BahnQuery
 
+import datetime
+
+def next_monday():
+    today = datetime.date.today()
+    nextmonday = today + datetime.timedelta(days=-today.weekday(), weeks=1)
+    return nextmonday
 
 class ZentraleOrte(Tool):
     _param_projectname = 'projectname'
     _workspace = 'FGDB_Erreichbarkeit.gdb'
     
     def add_outputs(self):
-        pass
+        
+        group_layer = ("erreichbarkeit")
+        fc = 'Haltestellen'
+        layer_nullfall = 'Haltestellen'
+
+        self.output.add_layer(group_layer, layer_nullfall, fc, zoom=False)
     
     def run(self):
+        self.query = BahnQuery(date=next_monday())
+        arcpy.AddMessage('Berechne die zentralen Orte und Haltestellen '
+                         'in der Umgebung...')
+        self.write_centers_stops()
+        arcpy.AddMessage('Ermittle die Anzahl der Abfahrten je Haltestelle...')
+        self.update_connections(projectarea_only=True)
+        
+    def write_centers_stops(self):
+        '''get centers in radius around project centroid, write their closest
+        stops and the stops near the project to the db
+        '''
+        # truncate tables, will be filled in progress
+        self.parent_tbx.delete_rows_in_table('Zentrale_Orte')
+        self.parent_tbx.delete_rows_in_table('Haltestellen')        
+        
         centroid = get_project_centroid(self.par.projectname.value)
         df_central = self.parent_tbx.table_to_dataframe(
             'Zentrale_Orte_Neu', 
@@ -35,38 +61,73 @@ class ZentraleOrte(Tool):
         df_oz_within = df_oz[oz_within]
         df_mz_within = df_mz[mz_within]
         
-        query = BahnQuery()
-        
-        def get_stations(points):
-            stations = []
+        def get_closest_stops(points):
+            stops = []
             for point in points:
                 t_p = Point(point[0], point[1],
                             epsg=self.parent_tbx.config.epsg)
                 t_p.transform(4326)
-                station = query.closest_station((t_p.x, t_p.y))
-                stations.append(station)
-            return stations
+                closest = self.query.stops_near(t_p, n=1)[0]
+                stops.append(closest)
+            return stops
             
-        oz_stations = get_stations(oz_points)
-        mz_stations = get_stations(mz_points)
-        df_oz_within['haltestelle'] = oz_stations
-        df_mz_within['haltestelle'] = mz_stations
+        oz_stops = get_closest_stops(oz_points)
+        mz_stops = get_closest_stops(mz_points)
+        
+        df_oz_within['id_haltestelle'] = [s.id for s in oz_stops]
+        df_mz_within['id_haltestelle'] = [s.id for s in mz_stops]
                 
         df_within = pd.concat([df_oz_within, df_mz_within])
         df_within['name'] = df_within['GEN']
         df_within['id_zentraler_ort'] = df_within['OBJECTID']
         df_within['distanz'] = 0
-            
-        self.parent_tbx.delete_rows_in_table('Zentrale_Orte')
+        
         self.parent_tbx.insert_dataframe_in_table('Zentrale_Orte', df_within)
         
         p_centroid = Point(centroid[0], centroid[1],
                            epsg=self.parent_tbx.config.epsg)
         p_centroid.transform(4326)
-        tfl_station = get_stations([centroid])[0]
+        tfl_stops = self.query.stops_near(p_centroid, n=10)
         
+        self._stops_to_db(oz_stops)
+        self._stops_to_db(mz_stops)
+        self._stops_to_db(tfl_stops, is_project_stop=1)
         
+    def update_connections(self, projectarea_only=False):
+        '''update the db-column 'abfahrten' of the stops with the number
+        of departures
+        '''
+        where = 'flaechenzugehoerig = 1' if projectarea_only else None
+        df_stops = self.parent_tbx.table_to_dataframe('Haltestellen',
+                                                      where=where)
+        ids = df_stops['id'].values
+        n_departures = self.query.n_departures(ids)
+        df_stops['abfahrten'] = n_departures
+        self.parent_tbx.dataframe_to_table('Haltestellen', df_stops, ['id'])
         
+    def _stops_to_db(self, stops, is_project_stop=0):
+        '''(warning: changes projection of point!)'''
+        table = self.folders.get_table('Haltestellen')
+        ids = []
+        names = []
+        shapes = []
+        
+        for stop in stops:
+            stop.transform(self.parent_tbx.config.epsg)
+            shapes.append(arcpy.Point(stop.x, stop.y))
+            ids.append(stop.id)
+            names.append(stop.name)
+            
+        column_values = {
+            'SHAPE': shapes,
+            'id': ids,
+            'name': names,
+            'flaechenzugehoerig': [is_project_stop] * len(stops), 
+            'abfahrten': [0] * len(stops)
+        }
+        self.parent_tbx.insert_rows_in_table('Haltestellen',
+                                             column_values=column_values)
+
 
 class TbxZentraleOrteOEPNV(Tbx):
 
