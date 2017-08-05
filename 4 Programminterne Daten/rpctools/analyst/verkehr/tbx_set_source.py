34e93ae0c875eb7e5658859f5b5e6415d377dc60
Stefaan <hessmann.stefaan@googlemail.com>
2017-08-05 14:17:09 +0200

    removed xy-coord from dialog-box

--- 4 Programminterne Daten/rpctools/analyst/verkehr/tbx_set_source.py
+++ 4 Programminterne Daten/rpctools/analyst/verkehr/tbx_set_source.py
@@ -16,7 +16,7 @@ class SetSource(Tool):
     def add_outputs(self):
         # update the names of the areas the points belong to
         df_areas = self.parent_tbx.table_to_dataframe(
-            'Teilflaechen_Plangebiet', 
+            'Teilflaechen_Plangebiet',
             workspace='FGDB_Definition_Projekt.gdb',
             columns=['id_teilflaeche', 'Name']
         )
@@ -26,7 +26,7 @@ class SetSource(Tool):
         df_points.rename(columns={'Name': 'name_teilflaeche'}, inplace=True)
         self.parent_tbx.dataframe_to_table('Anbindungspunkte', df_points,
                                            ['id_teilflaeche'])
-        
+
         self.output.add_layer('verkehr', 'Anbindungspunkte',
                               featureclass='Anbindungspunkte',
                               template_folder='Verkehr')
@@ -34,8 +34,8 @@ class SetSource(Tool):
     def run(self):
         area, i = self.parent_tbx.get_selected_area()
         toolbox = self.parent_tbx
-        source_x = toolbox.par.x_source.value
-        source_y = toolbox.par.y_source.value
+        source_x = toolbox.x
+        source_y = toolbox.y
         shape = arcpy.Point(source_x, source_y)
         id = area['id_teilflaeche']
         toolbox.update_table('Anbindungspunkte',
@@ -72,35 +72,12 @@ class TbxSetSource(TbxFlaechendefinition):
 
     def _open(self, params):
         super(TbxSetSource, self)._open(params)
-        x, y = self.config.active_coord
-        params.x_source.value = x
-        params.y_source.value = y
-        params.x_source.enabled = False
-        params.y_source.enabled = False
+        self.x, self.y = self.config.active_coord
 
     def _getParameterInfo(self):
         params = super(TbxSetSource, self)._getParameterInfo()
-
-        # x coord
-        p = self.add_parameter('x_source')
-        p.name = u'x'
-        p.displayName = u'X-Koordinate des Anbindungspunktes'
-        p.parameterType = 'Required'
-        p.direction = 'Input'
-        p.datatype = 'GPDouble'
-        p.value = 0
-
-        # y coord
-        p = self.add_parameter('y_source')
-        p.name = u'y'
-        p.displayName = u'Y-Koordinate des Anbindungspunktes'
-        p.parameterType = 'Required'
-        p.direction = 'Input'
-        p.datatype = 'GPDouble'
-        p.value = 0
-
         return self.par
-    
+
     def set_selected_area(self):
         pass
 
